c4176ccb25eb8884efa6815bdc3553d935131ef2
Ryan Blakeman <rblakeman31@gmail.com>
2019-05-22 22:34:35 -0700

    Code formatted using Prettier

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import VideoListItem from './video_list_item';
+import React from 'react'
+import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
-    const videoItems = props.videos.map((video) => {
-        return (
-        <VideoListItem
-            onVideoSelect={props.onVideoSelect}
-            key={video.etag}
-            video={video}/>
-        );
-    });
-
+  const videoItems = props.videos.map((video) => {
     return (
-        <ul className="col-md-4 list-group">
-            {videoItems}
-        </ul>
-    );
-};
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}
+      />
+    )
+  })
 
-export default VideoList;
+  return <ul className="col-md-4 list-group">{videoItems}</ul>
+}
+
+export default VideoList
