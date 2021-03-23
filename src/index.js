223f5b61f78c12eb69575c13876be9c364af5564
Ryan Blakeman <rblakeman31@gmail.com>
2021-03-22 22:19:53 -0700

    refactored to use yarn and create-react-app bootstrap, upgraded packages to latest

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.css';
+import App from './components/app';
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.getElementById('root'))
