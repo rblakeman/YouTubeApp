5a1de47c76b410c095e3e6af5aefe79d035ff17a
Ryan Blakeman <rblakeman31@gmail.com>
2025-10-19 17:22:38 -0700

    add `cicd` workflow for automating code checks and `gh-pages` deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+    // Since this is deployed to `https://rblakeman.github.io/YouTubeApp/`
+    base: '/YouTubeApp/',
     plugins: [react()],
     server: {
         port: 3000,
